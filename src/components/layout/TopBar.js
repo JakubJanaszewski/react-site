9f9163cd2c4f48d77f2c160b22f6e1be206c0547
JakubJanaszewski <jakubjanaszewskii@gmail.com>
2022-04-02 13:50:04 +0200

    Styled Topbar

--- src/components/layout/TopBar.js
+++ src/components/layout/TopBar.js
@@ -1,18 +1,19 @@
 import { Link } from 'react-router-dom';
-//import classes from './TopBar.module.css';
+import classes from './TopBar.module.css';
 
 function TopBar() {
-return (
-    <header>
-        <div>React Site</div>
-        <nav>
-            <ul>
-                <li><Link to="/">mainPage</Link></li>
-                <li><Link to="/sing-up">Sing Up</Link></li>
-                <li><Link to="/log-in">Log In</Link></li>
-                <li><Link to="/favorites">Favorites</Link></li>
-            </ul>
-        </nav>
-    </header>);
+    return (
+        <header className={classes.header}>
+            <div className={classes.logo}>React Site</div>
+            <nav>
+                <ul>
+                    <li><Link to="/">Main Page</Link></li>
+                    <li><Link to="/sing-up">Sing Up</Link></li>
+                    <li><Link to="/log-in">Log In</Link></li>
+                    <li><Link to="/favorites">Favorites</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 export default TopBar;
