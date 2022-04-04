483e980ab2c41fb069777fc41b507ba225f3caf2
JakubJanaszewski <jakubjanaszewskii@gmail.com>
2022-04-04 19:10:32 +0200

    Authentication v2

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route, Routes, Navigate} from 'react-router-dom';
+import { useContext } from 'react';
+
+import './index.css';
+import MainPage from './pages/MainPage';
+import SignUpPage from './pages/SignUp';
+import SignInPage from './pages/SignIn';
+import FavoritesPage from './pages/Favorites';
+import OfferPage from './pages/Offer';
+import AccountContext from './context/account-context';
+
+function App() {
+    const accContext = useContext(AccountContext);
+
+    return (
+        <Routes>
+            <Route exact path='/'       element = {<MainPage/>}/>
+            <Route path='/sign-up'      element = {<SignUpPage/>}/>
+            <Route path='/sign-in'      element = {<SignInPage/>}/>
+            <Route path='/favorites'    element = {(accContext.isSignedIn) ? <FavoritesPage/> : <Navigate to='/'/>}/>
+            <Route path='/offer'        element = {<OfferPage/>}/>
+        </Routes>
+    );
+  }
+  
+  export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
-import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 
 import './index.css';
-import MainPage from './pages/MainPage';
-import SignUpPage from './pages/SignUp';
-import SignInPage from './pages/SignIn';
-import FavoritesPage from './pages/Favorites';
-import OfferPage from './pages/Offer';
+import App from './App';
 import Layout from './components/layout/Layout';
 import { FavoritesContextProvider } from './context/favorites-context';
 import { AccountContextProvider } from './context/account-context';
-import AccountContext from './context/account-context';
 
 const container = document.getElementById('root');
 const root = ReactDOMClient.createRoot(container);
@@ -20,13 +15,7 @@ root.render(
         <FavoritesContextProvider>
             <BrowserRouter>
                 <Layout>
-                    <Routes>
-                        <Route exact path='/'       element = {<MainPage/>}/>
-                        <Route path='/sign-up'      element = {<SignUpPage/>}/>
-                        <Route path='/sign-in'      element = {<SignInPage/>}/>
-                        <Route path='/favorites'    element = {(AccountContext.isSignedIn) ? <FavoritesPage/> : <Navigate to='/'/>}/>
-                        <Route path='/offer'        element = {<OfferPage/>}/>
-                    </Routes>
+                    <App/>
                 </Layout>
             </BrowserRouter>
         </FavoritesContextProvider>
