c0c14f12298172c02d92acd8ddb6adab4fea541b
JakubJanaszewski <jakubjanaszewskii@gmail.com>
2022-05-10 14:35:36 +0200

    Update SignUpForm.js

--- src/components/account/SignUpForm.js
+++ src/components/account/SignUpForm.js
@@ -51,7 +51,7 @@ function SignUpForm(props) {
       password: enteredpassword,
       firstName: enteredFirstName,
       lastName: enteredLastName,
-      phone: enteredPhoneNumber,
+      phoneNumber: enteredPhoneNumber,
       dateOfBirth: enteredDateOfBirth,
       adressString: enteredStreetInput,
       zipCode: enteredZipCode,
